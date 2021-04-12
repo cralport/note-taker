4a5886a9f2e40c9ac7831a471dd0663290c808da
cory alport <cralport@gmail.com>
2021-04-12 02:59:04 -0600

    added some routes

--- server.js
+++ server.js
@@ -1,28 +1,50 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { RSA_NO_PADDING } = require('constants');
+
+
 const app = express();
+const PORT = process.env.PORT || 3001;
+const mainDir = path.join(__dirname, '/public');
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-const path = require('path');
-let jsonData = require('./db.json');
-console.log(jsonData);
-const PORT = process.env.PORT || 3001;
-
-// app.get('/api/animals', (req, res) => {
-//     let results = animals;
-//     if (req.query) {
-//         results = filterByQuery(req.query, results);
-//     }
-//     res.json(results);
-// });
-
-app.get('/api/notes', (req, res) => {
-    res.json(jsonData);
-})
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(mainDir, 'notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(mainDir, 'index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    let newNote = req.body;
+    let uniqueId = (savedNotes.length).toString();
+    newNote = uniqueId;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content:" newNote);
+    res.json(savedNotes);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
 })
+
 
 app.post('/api/notes', (req, res) => {
     let stuff = req.body
